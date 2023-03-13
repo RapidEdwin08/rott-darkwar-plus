3748226d4a66f90939ea1fc406f90e42fbbe800b
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-13 10:52:42 -0400

    Update rott-darkwar_plus.sh

--- rott-darkwar_plus.sh
+++ rott-darkwar_plus.sh
@@ -18,8 +18,7 @@ echo "            Rise Of The Triad: Dark War / The Hunt Begins
                    .|||    ||||||||||||    ||||.   
                   ||||    ||||||||||||||    |||||  
                  ||||||||||||||||||||||||||||||||| 
-"
-)
+")
 
 rottDWsh=$(
 echo '#!/bin/bash
@@ -73,17 +72,23 @@ rottZIPmod=/home/pi/RetroPie/roms/ports/rott-darkwar/rott-extreme.zip
 
 # Default Emulator Entries for RetroPie RiseOfTheTriad DarkWar
 [rott-darkwar]: pushd ~/..ports/rott-darkwar/; rott-darkwar
-[rott-darkwar-XINIT]: XINIT:pushd ~/..ports/rott-darkwar/; rott-darkwar
+[rott-darkwar-XINIT]: ./ports/rott-darkwar/rott-darkwar.sh
 
 # rott-darkwar+ Emulator Entries for RetroPie RiseOfTheTriad DarkWar
 [rott-darkwar+]: pushd TMPFS/; rott-darkwar
-[rott-darkwar+XINIT]: XINIT:pushd TMPFS/; rott-darkwar
+[rott-darkwar+XINIT]: ./configs/ports/rott-darkwar/rott-darkwar-plus.sh
 
 ======================================================================
 CURRENT CONTENT [/opt/retropie/configs/ports/rott-darkwar/emulators.cfg]:                   
 ======================================================================
-'
-)
+')
+
+rottREFShb=$(
+echo '
+======================================================================
+CURRENT CONTENT [/opt/retropie/configs/ports/rott-huntbgin/emulators.cfg]:                   
+======================================================================
+')
 
 mainMENU()
 {
@@ -93,21 +98,23 @@ if [ ! -f /opt/retropie/configs/ports/rott-darkwar/emulators.cfg ]; then
 	dialog --no-collapse --title "***N0TICE*** [..ports/rott-darkwar/emlators.cfg] NOT FOUND!" --ok-label MENU --msgbox "You MUST INSTALL RiseOfTheTriad DarkWar from RetroPie Setup 1st! [rott-darkwar]"  25 75
 fi
 # Confirm Configurations
-confROTTplus=$(dialog --stdout --no-collapse --title " [rott-darkwar-plus] for RetroPie by: RapidEdwin08 [v$version]" \
+confROTTplus=$(dialog --no-collapse --title " [rott-darkwar-plus] for RetroPie by: RapidEdwin08 [v$version]" \
 	--ok-label OK --cancel-label EXIT \
 	--menu "$rottLOGO" 25 75 20 \
 	1 "><  INSTALL [rott-darkwar+] for RetroPie ><" \
 	2 "><  REMOVE  [rott-darkwar+] for RetroPie ><" \
 	3 "><  UPDATE  [rott-huntbgin] +/- XINIT Entries for RetroPie ><" \
-	R "><  REFERENCES  ><")
+	4 "><  GET [rott-darkwar] + [rott-huntbgin] P0RTs for RetroPie ><" \
+	S "><  OPEN [RetroPie-Setup] ><" \
+	R "><  REFERENCES  ><" 2>&1>/dev/tty)
 
 # INSTALL [rott-darkwar+]
 if [ "$confROTTplus" == '1' ]; then
-	confINSTALLrottplus=$(dialog --stdout --no-collapse --title "               INSTALL [rott-darkwar+] for RetroPie             " \
+	confINSTALLrottplus=$(dialog --no-collapse --title "               INSTALL [rott-darkwar+] for RetroPie             " \
 		--ok-label OK --cancel-label BACK \
-		--menu "                          ? ARE YOU SURE ?             \n               [rott-darkwar+]  [rott-darkwar+XINIT]" 25 75 20 \
+		--menu "                          ? ARE YOU SURE ?             \n                [rott-darkwar]  [rott-darkwar-XINIT] \n               [rott-darkwar+]   [rott-darkwar+XINIT]" 25 75 20 \
 		1 "><  YES INSTALL [rott-darkwar+] for RetroPie  ><" \
-		2 "><  BACK  ><")
+		2 "><  BACK  ><" 2>&1>/dev/tty)
 	# Install Confirmed - Otherwise Back to Main Menu
 	if [ "$confINSTALLrottplus" == '1' ]; then
 		tput reset
@@ -163,11 +170,11 @@ fi
 
 # REMOVE [rott-darkwar+]
 if [ "$confROTTplus" == '2' ]; then
-	confREMOVErottplus=$(dialog --stdout --no-collapse --title "          REMOVE [rott-darkwar+] for RetroPie              " \
+	confREMOVErottplus=$(dialog --no-collapse --title "          REMOVE [rott-darkwar+] for RetroPie              " \
 		--ok-label OK --cancel-label BACK \
 		--menu "                          ? ARE YOU SURE ?             " 25 75 20 \
 		1 "><  YES REMOVE [rott-darkwar+] for RetroPie  ><" \
-		2 "><  BACK  ><")
+		2 "><  BACK  ><" 2>&1>/dev/tty)
 	# Remove Confirmed - Otherwise Back to Main Menu
 	if [ "$confREMOVErottplus" == '1' ]; then
 		tput reset
@@ -203,11 +210,11 @@ if [ "$confROTTplus" == '3' ]; then
 		dialog --no-collapse --title "***N0TICE*** [..ports/rott-huntbgin/emlators.cfg] NOT FOUND!" --ok-label MENU --msgbox "You MUST INSTALL RiseOfTheTriad HuntBegins from RetroPie Setup 1st! [rott-huntbgin]"  25 75
 	fi
 	
-	confINSTALLrottplusHB=$(dialog --stdout --no-collapse --title "     UPDATE  [rott-huntbgin] for RetroPie +/- XINIT        " \
+	confINSTALLrottplusHB=$(dialog --no-collapse --title "     UPDATE  [rott-huntbgin] for RetroPie +/- XINIT        " \
 		--ok-label OK --cancel-label BACK \
-		--menu "                          ? ARE YOU SURE ?             \n               [rott-huntbgin]  [rott-huntbgin-XINIT]" 25 75 20 \
+		--menu "                          ? ARE YOU SURE ?             \n \n       Update [rott-huntbgin] to include BOTH +/- XINIT entries \n \n      [emulators.cfg] -> [rott-huntbgin]  [rott-huntbgin-XINIT]" 25 75 20 \
 		1 "><  YES UPDATE  [rott-huntbgin] +/- XINIT Entries for RetroPie ><" \
-		2 "><  BACK  ><")
+		2 "><  BACK  ><" 2>&1>/dev/tty)
 	# Install Confirmed - Otherwise Back to Main Menu
 	if [ "$confINSTALLrottplusHB" == '1' ]; then
 		tput reset
@@ -236,9 +243,53 @@ if [ "$confROTTplus" == '3' ]; then
 	mainMENU
 fi
 
+# GET [rott-darkwar] + [rott-huntbgin] from GIT
+if [ "$confROTTplus" == '4' ]; then
+	gitROTTports=$(dialog --no-collapse --title "           GET [rott-darkwar] + [rott-huntbgin] P0RTs for RetroPie             " \
+		--ok-label OK --cancel-label BACK \
+		--menu "                          ? ARE YOU SURE ?             \n \n This will GIT/UPDATE/OVERWRITE [rott] scriptmodules in RetroPie-Setup \n \n  /home/$USER/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules/ports/* \n \n             https://github.com/Exarkuniv/RetroPie-Extra/          " 25 75 20 \
+		1 "><  YES GET [rott-darkwar] + [rott-huntbgin] P0RTs for RetroPie ><" \
+		2 "><  BACK  ><" 2>&1>/dev/tty)
+	# Install Confirmed - Otherwise Back to Main Menu
+	if [ "$gitROTTports" == '1' ]; then
+		tput reset
+		# GIT ROTT P0RTs - Check If Internet Connection Available
+		wget -q --spider http://google.com
+		if [ $? -eq 0 ]; then
+			mkdir ~/RetroPie-Setup/ext/RetroPie-Extra > /dev/null 2>&1
+			mkdir ~/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules > /dev/null 2>&1
+			mkdir ~/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules/ports > /dev/null 2>&1
+			wget https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/ports/rott-darkwar.sh -P /dev/shm/; mv /dev/shm/rott-darkwar.sh ~/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules/ports/
+			wget https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/scriptmodules/ports/rott-huntbgin.sh -P /dev/shm/; mv /dev/shm/rott-huntbgin.sh ~/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules/ports/
+		else
+			# No Internet - Back to Main Menu
+			dialog --no-collapse --title "               [ERROR]               " --msgbox "   *INTERNET CONNECTION REQUIRED* TO GIT ROTT P0RTs"  25 75
+			mainMENU
+		fi
+		dialog --no-collapse --title " GET [rott-darkwar] + [rott-huntbgin] P0RTs for RetroPie FINISHED" --ok-label Back --msgbox "  RetroPie Setup -> Manage Packages -> Manage experimental packages"  25 75
+		mainMENU
+	fi
+	mainMENU
+fi
+
+# Open RPSetup
+if [ "$confROTTplus" == 'S' ]; then
+	confiRPsetup=$(dialog --no-collapse --title " RetroPie Setup -> Manage Packages -> Experimental Packages" \
+		--ok-label OK --cancel-label Back \
+		--menu "                          ? ARE YOU SURE ?              \n $(ls ~/RetroPie-Setup/ext/RetroPie-Extra/scriptmodules/ports/ | grep 'rott-' )" 25 75 20 \
+		Y "YES OPEN [RetroPie-Setup]" \
+		B "BACK" 2>&1>/dev/tty)
+	if [ "$confiRPsetup" == 'Y' ]; then
+		cd ~/RetroPie-Setup/ && sudo bash ~/RetroPie-Setup/retropie_setup.sh
+		#sudo bash ~/RetroPie-Setup/retropie_packages.sh retropiemenu launch "/home/$USER/RetroPie-Setup/retropie_setup.sh" </dev/tty > /dev/tty
+		exit 0
+	fi
+	mainMENU
+fi
+
 # REFERENCES
 if [ "$confROTTplus" == 'R' ]; then	
-	dialog --no-collapse --title " REFERENCES" --ok-label Back --msgbox "$rottREFS $(cat /opt/retropie/configs/ports/rott-darkwar/emulators.cfg)"  25 75
+	dialog --no-collapse --title " REFERENCES" --ok-label Back --msgbox "$rottREFS $(cat /opt/retropie/configs/ports/rott-darkwar/emulators.cfg) $rottREFShb $(cat /opt/retropie/configs/ports/rott-huntbgin/emulators.cfg)"  25 75
 	mainMENU
 fi
 
